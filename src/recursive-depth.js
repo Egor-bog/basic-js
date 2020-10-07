dedc0ae4ce3ca08d21072d7310e7c554ff7b0e70
Egor-bog <7548494@gmail.com>
2020-10-07 19:17:58 +0300

    fix: test

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -4,13 +4,15 @@ module.exports = class DepthCalculator {
   calculateDepth(arr) {
     let level = 0;
     if (Array.isArray(arr)) {
-        if (arr.length == 0) level = 1;
+        if (arr.length == 0) {
+          level = 1;
+        }
         else {
-            arr.forEach(item => {
-                let levelItem = 1;
-                levelItem += this.calculateDepth(item);
-                if(levelItem > level) level = levelItem;
-            })
+          arr.forEach(item => {
+              let levelItem = 1;
+              levelItem += this.calculateDepth(item);
+              if(levelItem > level) level = levelItem;
+          })
         }
     }
     return level;
